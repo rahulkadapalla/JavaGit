aa0a187ddf3611899d2919c21d9440b4f0a1e39d
Akhil Kadapalla <akhilkadapalla@gmail.com>
2020-11-18 22:31:06 +0530

    divided by zero exception

--- JavaGit/src/com/git/BuildTest.java
+++ JavaGit/src/com/git/BuildTest.java
@@ -3,6 +3,8 @@ package com.git;
 public class BuildTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test build");
+		int a = 5/0;
+		
+		System.out.println(a);
 	}
 }
